c87fe7f614761b9121a600ffe9ec5e3c74e27501
Miquel Maxime <maxime.miquel@epitech.eu>
2024-05-09 18:27:43 +0200

    Refactor JobList component UI and add job details

--- components/JobList/JobList.vue
+++ components/JobList/JobList.vue
@@ -3,23 +3,31 @@
     <span class="loader"></span>
   </div>
   <div v-else class="flex flex-row justify-center flex-wrap pt-10" v-if="jobsList">
-    <div class="cursor-pointer w-[27rem] m-2 flex flex-col justify-around">
-      <a class="cursor-pointer w-full mb-4 p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100
-        dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="w-[27rem] m-2 flex flex-col justify-around">
+      <a class="w-full mb-4 p-5 bg-white border border-gray-200 rounded-lg shadow
+        dark:bg-gray-800 dark:border-gray-700">
 
         <h2 class="mb-2 text-4xl font-bold tracking-tight flex justify-between text-gray-900 dark:text-white">Offres
           d'emploi<span>🎓</span>
         </h2>
       </a>
 
-      <a class="cursor-pointer p-5 h-full flex flex-col justify-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100
+      <a class="cursor-pointer p-5 h-full flex flex-col justify-between bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100
         dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
         <h2 class="mb-2 w-[90%] text-2xl font-bold tracking-tight text-gray-900 dark:text-white">This website
           was design
           by <span class="text-lg-">Maxime Miquel</span></h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, inventore quo nemo vitae labore aperiam impedit
-          alias velit totam veritatis animi, atque laboriosam!</p>
+        <p class="font-normal text-gray-700 dark:text-gray-400 whitespace-pre-line">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque cupiditate aperiam maiores non veritatis
+          obcaecati odio, commodi quam tenetur laboriosam, laborum quaerat unde placeat magni!
+        </p>
+        <ul>
+          <li>
+            Nombres d'offres d'emploi : {{ jobsList.meta.pagination.total }}
+          </li>
+          <li></li>
+        </ul>
       </a>
     </div>
     <a v-for="job in jobsList.data"
